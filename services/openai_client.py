9b4e73f44a69ea103ca3aaf755edfa738c06c0ca
vlad <vladvoytkiv321@gmail.com>
2025-07-18 17:23:53 +0300

    feat: add dependency injection for a client, validation of status

--- services/openai_client.py
+++ services/openai_client.py
@@ -1,13 +1,15 @@
 from config import open_api_setting
 import httpx
-from typing import List
+from typing import List, Optional
+from fastapi import status, HTTPException
 
 
 class OpenAiService:
-    def __init__(self):
+    def __init__(self, client: Optional[httpx.AsyncClient] = None):
         self.model = open_api_setting.model
         self.api_key = open_api_setting.openai_key
         self.base_url = open_api_setting.base_url
+        self._client = client
 
     async def ask(self, messages: List[dict]) -> dict:
         header = {
@@ -18,7 +20,15 @@ class OpenAiService:
             'model': self.model,
             'messages': messages
         }
-        async with httpx.AsyncClient() as client:
-            response = await client.post(self.base_url, headers=header, json=json_data)
-            response.raise_for_status()
-            return response.json()
+
+        if self._client:
+            response = await self._client.post(self.base_url, headers=header, json=json_data)
+        else:
+            async with httpx.AsyncClient() as client:
+                response = await client.post(self.base_url, headers=header, json=json_data)
+                response.raise_for_status()
+
+        if response.status_code != status.HTTP_200_OK:
+            raise HTTPException(status_code=response.status_code, detail="OpenAI request failed")
+
+        return response.json()
