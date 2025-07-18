6b06406ef25d8d4cfe36e14cc2872f4a42e23020
vlad <vladvoytkiv321@gmail.com>
2025-07-18 17:25:52 +0300

    fix: rewrite configuration into new syntax

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class OpenAISettings(BaseSettings):
@@ -6,8 +6,7 @@ class OpenAISettings(BaseSettings):
     model: str
     base_url: str
 
-    class Config:
-        env_file = '.env'
+    model_config = SettingsConfigDict(env_file='.env')
 
 
 class Settings(BaseSettings):
