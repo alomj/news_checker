40c859f5f097f42e031310f5ad74aab2e3788381
vlad <vladvoytkiv321@gmail.com>
2025-07-25 22:38:25 +0300

    feat: implement endpoint

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from services.openai_client import OpenAiService
 from llm.query_generator import QueryGenerator
 from schema import SearchRequest, Request, SearchResponse
 from services.search_client import SearchService
-from utils.get_clients import get_search_engine
+from utils.get_clients import get_search_engine, get_llm_analyzer
+from llm.technical_analyzer import TechnicalAnalyzer
+from llm.credibility_analysis.llm_analyzer import LLMAnalyzer
 
 app = FastAPI(title=settings.app_title,
               description=settings.app_description
@@ -25,3 +27,11 @@ async def search_news(request: SearchRequest,
                       search_service: SearchService = Depends(get_search_engine)):
     items = await search_service.generate(request.headlines)
     return items
+
+
+@app.post('/rate-search', status_code=status.HTTP_201_CREATED, response_model=SearchResponse)
+async def rate_news(search_response: SearchResponse,
+                    llm_analyzer: LLMAnalyzer = Depends(get_llm_analyzer)):
+    technical_analyzer = TechnicalAnalyzer(llm_analyzer)
+    result = await technical_analyzer.analyze(search_response)
+    return result
