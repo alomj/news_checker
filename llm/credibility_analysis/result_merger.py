7fd48f76b7ac7f9fa937d0f524ed8de27182c78b
vlad <vladvoytkiv321@gmail.com>
2025-07-28 18:08:05 +0300

    fix remove redundant deep copy

--- llm/credibility_analysis/result_merger.py
+++ llm/credibility_analysis/result_merger.py
@@ -1,11 +1,10 @@
 from schema import SearchResponse
 from typing import List
-import copy
 
 
 class ResultMerger:
     def __init__(self, original_response: SearchResponse):
-        self.working_copy = copy.deepcopy(original_response)
+        self.response = original_response
 
     def merge(self, batches: List, metadata: List):
         all_results = []
@@ -24,9 +23,9 @@ class ResultMerger:
             query_idx = current_metadata['query_index']
             result_idx = current_metadata['result_index']
 
-            target_hit = self.working_copy.items[item_idx].queries[query_idx].results[result_idx]
+            target_hit = self.response.items[item_idx].queries[query_idx].results[result_idx]
 
             target_hit.credibility_score = current_score['credibility_score']
             target_hit.flags = current_score['flags']
 
-        return self.working_copy
+        return self.response
