364df10976597bf733ae1eb911ed8aad69d83596
vlad <vladvoytkiv321@gmail.com>
2025-07-28 16:53:50 +0300

    feat: separate limiting tasks, initializer Concurrency limiter class

--- llm/credibility_analysis/analysis_orchestrator.py
+++ llm/credibility_analysis/analysis_orchestrator.py
@@ -2,13 +2,13 @@ from llm.credibility_analysis.batch_processor import BatchProcessor
 from llm.credibility_analysis.llm_analyzer import LLMAnalyzer
 from llm.credibility_analysis.result_merger import ResultMerger
 from schema import SearchResponse
-import asyncio
+from utils.concurrency_limiter import ConcurrencyLimiter
 
 
 class AnalyzerOrchestrator:
-    def __init__(self, llm_analyzer: LLMAnalyzer,
-                 ):
+    def __init__(self, llm_analyzer: LLMAnalyzer):
         self.llm_analyzer = llm_analyzer
+        self.limiter = ConcurrencyLimiter()
 
     async def process(self, search_response: SearchResponse):
         batch_processor = BatchProcessor(search_response)
@@ -17,16 +17,7 @@ class AnalyzerOrchestrator:
         batches, metadata = batch_processor.process()
 
         tasks = [self.llm_analyzer.analyze_batch(batch) for batch in batches]
-
-        semaphore = asyncio.Semaphore(5)
-
-        async def limited_task(task):
-            async with semaphore:
-                return await task
-
-        limited_tasks = [limited_task(task) for task in tasks]
-        completed_tasks = await asyncio.gather(*limited_tasks)
-
+        completed_tasks = await self.limiter.execute_tasks(tasks)
         result = result_merger.merge(completed_tasks, metadata)
 
         return result
