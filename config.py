86956058aa29e44cf541dfd12669392b72d4fcfa
vlad <vladvoytkiv321@gmail.com>
2025-07-28 17:22:42 +0300

    fix styling

--- config.py
+++ config.py
@@ -8,16 +8,18 @@ class OpenAISettings(BaseSettings):
 
     model_config = SettingsConfigDict(env_file='.env')
 
+
 class ConcurrencyLimit(BaseSettings):
     max_limit: int = 5
+
 
 class SearchConfig(BaseSettings):
     max_search: int = 5
 
+
 class Settings(BaseSettings):
     app_title: str = 'Fake News Detector'
     app_description: str = 'Custom utility to validate whether your source is fake or not'
-
 
 
 def get_openai_settings():
--- services/search_providers/duckduckgo_provider.py
+++ services/search_providers/duckduckgo_provider.py
@@ -4,6 +4,7 @@ from ddgs import DDGS
 from interfaces.search_provider import SearchProvider
 from config import SearchConfig
 
+
 class DuckDuckGoSearchProvider(SearchProvider):
     def search(self, query: str) -> List[Dict]:
         with DDGS() as ddgs:
